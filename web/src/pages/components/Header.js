05c5f334ed1aa4f5f99ff70827a0b6d334a58e6c
xfeng <fx96@stu.pku.edu.cn>
2021-05-15 22:18:48 +0800

    change header & footer style

--- web/src/pages/components/Header.js
+++ web/src/pages/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/ONL-Logo.png';
 import '../../styles/header.css';
 
 export default function Header () {
   return (
     <div className="header-container">
+      <div className="logo-container">
         <img alt="img" className="logo" src={logo} />
+        <div className="header-title">OpenNetLab</div>
+      </div>
+      <div className="links">
+        <a href="https://opennetlab.org/about">ABOUT US</a>
+        <a href="https://opennetlab.org/join">JOIN US</a>
+      </div>
     </div>
   );
 }
